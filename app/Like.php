1f02947f2508c996097b6b9e3f3cab23564ea044
Rayan Mahanna <rayan.mahanna@devinci.fr>
2018-01-22 18:40:30 +0100

    merging to master

--- app/Like.php
+++ app/Like.php
@@ -9,19 +9,10 @@ class CommentVote extends Model
 {
 
 
-
-    protected $fillable = ['comment_id','user_id','vote'];
+    protected $fillable = ['comment_id', 'user_id', 'vote'];
 
     protected $table = "vote";
 
     public $timestamps = false;
 
-class Like extends Model
-{
-    public function user(){
-        return $this->belongsTo('App\User');
-    }
-    public function article(){
-        return $this->belongsTo('App\Article');
-    }
 }
